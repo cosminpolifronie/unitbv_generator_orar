e3d3009fa776f162aa929cdd7d3f9c9c135cd322
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2017-10-01 11:27:59 +0300

    replaced openpyxl with xlsxwriter for custom formatting support (openpyxl still used for reading)

--- unitbv_generator_orar/unitbv_generator_orar.py
+++ unitbv_generator_orar/unitbv_generator_orar.py
@@ -1,70 +1,82 @@
-from openpyxl import Workbook, worksheet
-from openpyxl.styles import Alignment, Border, Font, NamedStyle, Side
-
-# setari pt.  stilul celulelor
-__fontname = 'Calibri'
-__fontsize = 24
-__fontcolor = 'FF000000'
-__bordercolor = 'FF9B9B9B'
-__borderstyle = 'thick'
+import openpyxl
+import sys
+import xlsxwriter
 
 __header_time = ('8:00 – 9:50', '10:00 – 11:50', '12:00 – 13:50', '14:00 – 15:50', '16:00 – 17:50', '18:00 – 19:50', '20:00 – 21:50')
 __header_day = ('Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă')
 
-workbook = Workbook()
-worksheet = workbook.active
+__bold_format = None
 
-cellstyle = NamedStyle(name='cellstyle', 
-                       font = Font(name = __fontname, size = __fontsize, color = __fontcolor), 
-                       border = Border(left = Side(style = __borderstyle, color = __bordercolor),
-                           right = Side(style = __borderstyle, color = __bordercolor),
-                           top = Side(style = __borderstyle, color = __bordercolor),
-                           bottom = Side(style = __borderstyle, color = __bordercolor)),
-                       alignment = Alignment(horizontal = 'center',
-                           vertical = 'center'))
+def get_mergedcell_value(source, coords):
+    for range in source.merged_cell_ranges:
+        merged_cells = list(openpyxl.utils.rows_from_range(range))
+        for row in merged_cells:
+            if coords in row:
+                return source[merged_cells[0][0]].value
+    return source[coords].value
 
-# format pagina: A0 (46.8 inch x 33.1 inch) landscape
-worksheet.page_setup.fitToPage = True
-worksheet.page_setup.orientation = 'landscape'
-worksheet.page_setup.paperHeight = '46.8in'
-worksheet.page_setup.paperWidth = '33.1in'
+def generate_worksheet(worksheet, source, row, version):
+    # format pagina: ANSI E (44 inch x 34 inch) landscape
+    worksheet.set_landscape()
+    worksheet.set_paper(26)
 
-# inaltime header ora: 1.10 inch
-# latime header zi: 3 inch
-# inaltime camp: 5 inch (2.5 inch per rand, un camp fiind format din 2 randuri
-# combinate pentru a permite afisarea materiilor din zile impare/pare)
-# latime camp: 6 inch (3 inch per coloana, aceeasi poveste ca mai sus, pentru a
-# permite afisarea materiilor care se desfasoara in acelasi timp)
-# inaltimea e in puncte (1 punct = 1/72 inch)
-# latimea e in numarul de caractere care incap in acel camp folosind fontul
-# standard
-worksheet.row_dimensions[1].height = 79.2
-for i in range(2, len(__header_day) * 2 + 2):
-    worksheet.row_dimensions[i].height = 180
-for i in range(0, len(__header_time) * 2 + 1):
-    worksheet.column_dimensions[chr(ord('A') + i)].width = 39
+    # inaltime header ora: 1.10 inch
+    # latime header zi: 3 inch
+    # inaltime camp: 5 inch (2.5 inch per rand, un camp fiind format din 2 randuri combinate pentru a permite afisarea materiilor din zile impare/pare)
+    # latime camp: 5.62 inch (2.81 inch per coloana, aceeasi poveste ca mai sus, pentru a permite afisarea materiilor care se desfasoara in acelasi timp) 
+    # inaltimea e in puncte (1 punct = 1/72 inch)
+    # latimea e in numarul de caractere care incap in acel camp folosind fontul standard
+    worksheet.set_column(0, len(__header_time) * 2, 36)
+    worksheet.set_row(0, 79.2)
+    for i in range(1, len(__header_day) * 2 + 1):
+        worksheet.set_row(i, 180)
 
-# generam headerele orarului
-# interval: luni-sambata, 8:00-21:50
-# populam orele si legam casutele headerului 2 cate 2
-for i in range(0, len(__header_time)):
-    _letter = chr(ord('B') + i * 2)
-    worksheet[_letter + '1'] = __header_time[i]
-    worksheet.merge_cells(_letter + '1:' + chr(ord(_letter) + 1) + '1')
+    # generam headerele orarului
 
-# populam zilele si legam casutele headerului 2 cate 2
-for i in range(1, len(__header_day) + 1):
-    _number = str(i * 2)
-    worksheet['A' + _number] = __header_day[i - 1]
-    worksheet.merge_cells('A' + _number + ':A' + str(int(_number) + 1))
+    # interval: luni-sambata, 8:00-21:50
+    # populam orele si legam casutele headerului 2 cate 2
+    for i in range(0, len(__header_time)):
+        worksheet.merge_range(0, 2 * i + 1, 0, 2 * i + 2, __header_time[i], __bold_format)
+    # populam zilele si legam casutele headerului 2 cate 2
+    for i in range(0, len(__header_day)):
+        worksheet.merge_range(2 * i + 1, 0, 2 * i + 2, 0, __header_day[i], __bold_format)
 
-# setam stilurile celulelor
-for i in range(1, len(__header_day) * 2 + 2):
-    for j in range(1, len(__header_time) * 2 + 2):
-        _letter = chr(ord('A') + j - 1)
-        worksheet[_letter + str(i)].style = cellstyle
+    # populam campul A1 cu detalii importante
+    # E1 = cod orar
+    # J3 = an universitar
+    worksheet.write_string(0, 0, str(get_mergedcell_value(source, 'E1')).replace(' ', '') + ' – ' + str(version).replace(' ', '') + '\n' + str(get_mergedcell_value(source, 'J3')).replace(' ', '') + '\n' + worksheet.get_name().replace('-', ' – '), __bold_format)
 
-# citim grupa
+    # populam campurile cu continut
 
-# salvam fisierul
-workbook.save('nume.xlsx')
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit('Mai putin de 3 argumente!\nFolosire: unitbv_generator_orar.py\n\tcale_orar\n\tfolder_output\n\tprimul_rand_al_grupei_de_generat\n\tprimul_rand_al_grupei_de_generat\n\tetc.')
+    
+    # setam sursa orarului
+    source = openpyxl.load_workbook(sys.argv[1]).active
+        
+    # generam workbook-ul curent
+    workbook = xlsxwriter.Workbook(sys.argv[2] + '\\orar-' + str(get_mergedcell_value(source, 'E1')).replace(' ', '') + '.xlsx')
+
+    __bold_format = workbook.add_format({
+                'font_name': 'Calibri',
+                'font_size': 22,
+                'bold': True,
+                'align': 'center',
+                'valign': 'vcenter',
+                'border': 5,
+                'border_color': '#9B9B9B'
+            })
+    
+    # extragem versiunea orarului
+    version = sys.argv[1].split('-')
+    version = version[len(version) - 1].split('.')[0].replace(' ', '')
+    # pentru fiecare rand oferit ca parametru generam un nou worksheet in
+    # fisier
+    for row in sys.argv[3:]:
+        worksheet = workbook.add_worksheet(str(str(get_mergedcell_value(source, 'A' + str(row))) + '-' + str(get_mergedcell_value(source, 'B' + str(row))) + '-' + str(get_mergedcell_value(source, 'C' + str(row)))).replace(' ', ''))
+        generate_worksheet(worksheet, source, row-1, version)
+    
+    # salvam fisierul
+    workbook.close()
